bb110f97dcabfffe12ac36ad307301836a114634
Baptiste-mrch <62337736+Baptiste-mrch@users.noreply.github.com>
2024-02-09 14:01:48 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 
-	echo("HELLO FROM PHP FROM GIT FROM STAGING V2. This is successfully updated V4 ! Test du CRON");
+	echo("HELLO FROM PHP FROM GIT FROM STAGING V2. This is successfully updated V4 ! Test du CRON2");
 	
 ?>
