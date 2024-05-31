76f10dfe6fc2c74c6585aec9b984ab7dc8247443
Baptiste-mrch <62337736+Baptiste-mrch@users.noreply.github.com>
2024-05-31 14:04:42 +0200

    Add files via upload

--- vue/vue_reponse.php
+++ vue/vue_reponse.php
@@ -1,37 +1,39 @@
 <h1>Résultat du QCM</h1>
 <?php
-    $creds_data = json_decode(file_get_contents('creds.json'), true);
-    $key = $creds_data['bdd'];
-    
-    $id = new mysqli("db","user",$key,"itsafecampus");
-    
+    $id = mysqli_connect("localhost","root","","itsafecampus");
     $note = 0;
     
     foreach($_POST as $id_question=>$id_reponse){
         $req = "select * from quizz_reponse where id_reponse=$id_reponse and verite = 1";
-        $res = mysqli_query($id,$rep);
+        $res = mysqli_query($id,$req);
+
         if(mysqli_num_rows($res)>0){
             $note = $note + 4;
         }else{
             ?>
-            <p class="color">Tu t'es planté à la question <?=$id_question?> : </p>
+            <div class="reponse">
+
+            
+            <h3><u>Tu t'es planté à la question :</u> </h3>
 
             <?php
-            $req2="select * from quizz_question where id_question=$id_question";
+            $req2="select * from quizz_question where id_question='$id_question'";
             $res2=mysqli_query($id,$req2);
             $ligne=mysqli_fetch_assoc($res2);
             ?>
                 <p class="question_error"><?=$ligne['libelleQ']?></p>
-                <p class="color">Il fallair répondre</p>
+                <h4><u>Il fallait répondre :</u></h4>
             <?php
 
-                $req3="select * from quizz_reponse where id_question=$id_question and verite=1";
+                $req3="select * from quizz_reponse where id_question='$id_question' and verite=1";
                 $res3=mysqli_query($id,$req3);
-                $lign3=mysqli_fetch_assoc($res3);
+                $ligne3=mysqli_fetch_assoc($res3);
 
             ?>
-            <p><?=$ligne3['libelleR']?></p>
+                <p><?=$ligne3['libelleR']?></p>
+            </div>
             <?php
         }
     }
 ?>
+<center><h2>Tu as eu <?=$note?>/20</h2></center>
