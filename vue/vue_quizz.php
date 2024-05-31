76abc0a59c599406ed5551f729072d1462f8a56c
Maarine-vyt <71020060+Maarine-vyt@users.noreply.github.com>
2024-05-31 13:27:41 +0200

    Add files via upload

--- vue/vue_quizz.php
+++ vue/vue_quizz.php
@@ -5,11 +5,11 @@
         $id_thematique = $_GET['id_thematique'];
         $id_formation = $_GET['id_formation'];
 
-
-        $creds_data = json_decode(file_get_contents('creds.json'), true);
-        $key = $creds_data['bdd'];
+        //$creds_data = json_decode(file_get_contents('creds.json'), true);
+        //$key = $creds_data['bdd'];
         
-        $id = new mysqli("db","user",$key,"itsafecampus");
+        //$id = new mysqli("db","user",$key,"itsafecampus");
+        $id = mysqli_connect("localhost","root","","itsafecampus");
 
 ?>
     <!-- Conteneur du quizz -->
@@ -24,64 +24,57 @@
                 ?>
                     <h2>Quizz <?php echo $uneFormation['nom_formation'];?></h2><hr>
                 
-                    <form method="post" action="" id="quizz_form">
+                    <form method="post" action="index.php?page=11" id="quizz_form">
                         <div class="formulaire_question">
-                        
-                            <!-- Appel la fonction selectWhereQuizz en fonction de l'id formation -->
-                            <?php
-                                $lesQuizz = $unControleur -> selectWhereQuizz($id_formation);
-                                //boucles sur toute les données dans la fonction pour afficher les données du
-                                //quizz en fonction de la formation réalisée
-                                foreach ($lesQuizz as $unQuizz) {
-                            ?>
-                                <form method="post" action="" id="quizz_form">
-                                    <div class="formulaire_question">
-                                        <label><?php echo $unQuizz['libelleQ']; ?></label><hr>
-                                        <?php
-                                            $req = "select * from quizz_question where id_formation = $id_formation";
-                                            $res = mysqli_query($id,$req);
-                                            while($ligne = mysqli_fetch_assoc($res)){
-                                                $id_question = $ligne['id_question'];
-                                        ?>
-                                                        
-                                                <label><?php $ligne['libelleQ'] ?></label>
-                                        <?php
-                                            }
-                                        ?>
-                                        <?php
+                            <!--<form method="post" action="" id="quizz_form">
+                                <div class="formulaire_question"> -->
+                                    <?php
+                                        $req = "select * from quizz_question where id_formation = $id_formation";
+                                        $res = mysqli_query($id,$req);
+                                        while($ligne = mysqli_fetch_assoc($res)){
+                                            $id_question = $ligne['id_question'];
+                                            $libelleQ = $ligne['libelleQ'];
+                                            ?>
+                                                <br><label><?php echo $libelleQ ?></label><hr><br>
+
+                                            <?php
                                             $req2="SELECT * from quizz_reponse where id_question = $id_question" ;
                                             $res2 = mysqli_query($id,$req2);
                                             while($ligne2 = mysqli_fetch_assoc($res2)){
                                         ?>
-                                                <input type="radio" id="radiobtn" name="" value="<?php echo $ligne2['libelleR'];?>">
+                                                <input type="radio" id="<?php $ligne2['id_reponse'] ?>" name="<?=$id_question?>" value="<?php echo $ligne2['id_reponse'];?>">
                                                 <span><?php echo $ligne2['libelleR'];?></label></span><br><br>
-                                        <?php
+                                    <?php
                                             }
-                                        ?>
-                                    </div>
-                                </form>
-                            <?php
-                                }
-                            ?>
+                                        }
+                                    ?>
+                                <!--</div>
+                            </form>-->
+
+                            <!--<div class="fiche_contact" style="align-items: center;">
+                                <div id="connect_retour" class="button_connect">
+                                    <input type="submit" class="bouttonCommencer" style="width: 100%;" value="Envoyez mes réponses">
+                                </div>
+                            </div> -->
                         </div>
+
+                            <!-- Bouton qui renvoie les données du formulaire et appel la fonction ??? qui va verifier
+                            si les réponses son bonne dans la BDD via quizz.php -->
+                            <div class="fiche_contact" style="align-items: center;">
+                                <div id="connect_retour" class="button_connect">
+                                    <a href="#" class="link2" onclick="document.getElementById('quizz_form').submit();">
+                                        <button class="bouttonCommencer" style="width: 100%;">Envoyer mes réponses</button>
+                                    </a>
+                                </div>
+                            </div>
+                        
                     </form>
                 <?php
                     }
                 ?>
             </div>
-            
-            <!-- Bouton qui renvoie les données du formulaire et appel la fonction ??? qui va verifier
-                        si les réponses son bonne dans la BDD via quizz.php -->
-            <div class="fiche_contact" style="align-items: center;">
-                <div id="connect_retour" class="button_connect">
-                    <a href="index.php?page=11" class="link2" onclick="document.getElementById('quizz_form').submit();">
-                        <button class="bouttonCommencer" style="width: 100%;">Envoyer mes réponses</button>
-                    </a>
-                </div>
-            </div>
         </div>
     </div>
 <?php
     }
-
 ?>
